69b8abbe2a43e372a616890f62c414b8f155dac6
RiddhiLokhande <riddhilokhande7@gmail.com>
2024-08-02 00:00:55 +0530

    Update exp 2 C.cpp

--- exp 2 C.cpp
+++ exp 2 C.cpp
@@ -5,18 +5,24 @@
 #include <iostream> 
 using namespace std;
 
+#include<iostream>
+using namespace std;
+
 int main() {
     int a;
-    cout<<"enter any interger: ";
+    cout<<"Enter a number: ";                       
     cin>>a;
-    cout<<"\ninteger = "<<a<<" and size is "<<sizeof(a)<<" bytes";
-    int register b;
-    cout<<"\nenter a number: ";
+    cout<<"\nInteger= "<<a<<" and size is "<<sizeof(a)<<" bytes.";  
+
+    register int b;
+    cout<<"\nEnter a number: ";                                     
     cin>>b;
-    cout<<"\nREGISTER = "<<b<<" and size is "<<sizeof(b)<<" bytes";
-    int auto c;
-    cout<<"\nenter an interger: ";
-    cin>> c;
-    cout<<"\nAuto ="<<c<<" and size is "<<sizeof(c)<<" bytes";
-    return 0;
+    cout<<"\nRegister= "<<b<<" and size is "<<sizeof(b)<<" bytes.";   
+
+    static int d;
+    cout<<"\nEnter any number: ";                                      
+    cin>>d;
+    cout<<"\nStatic= "<<d<<" and size is "<<sizeof(d)<<" bytes.";      
+
+    return 0;
 }
